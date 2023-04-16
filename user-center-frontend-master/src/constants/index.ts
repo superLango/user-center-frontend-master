586985513d303d51575e0177837c0d8062c4ed20
Lango <whl2644992002@gmail.com>
2023-04-16 11:16:05 +0800

    user-center-fronted-master first commit

---
+++ user-center-frontend-master/src/constants/index.ts
@@ -0,0 +1,6 @@
+export const SYSTEM_LOGO = "https://img2.baidu.com/it/u=1497007435,3369633696&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=500";
+
+/**
+ * 知识星球文档地址
+ */
+export const PLANET_LINK = "https://www.zsxq.com/";
