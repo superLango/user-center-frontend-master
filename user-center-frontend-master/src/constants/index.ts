d55a1fc1ae3b11ea7d7dd1eb502c16f21e67aa53
Lango <whl2644992002@gmail.com>
2023-05-05 11:14:54 +0800

    end

--- user-center-frontend-master/src/constants/index.ts
+++
@@ -1,6 +0,0 @@
-export const SYSTEM_LOGO = "https://user-1317880157.cos.ap-beijing.myqcloud.com/user-center/usercenter.png";
-
-/**
- * 知识星球文档地址
- */
-export const PLANET_LINK = "https://www.zsxq.com/";
